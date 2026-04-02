f1b08c7ec6b19efaeb02ddeadeb9f6535b9a393c
Pyae Sone Han <pyaesonehan246810@gmail.com>
2026-04-02 13:57:20 +0700

    Initial Commit

---
+++ SabaiSplit/SabaiSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabaiSplitApp.swift
+//  SabaiSplit
+//
+//  Created by Dylan on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SabaiSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
