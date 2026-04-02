66df02fa8b8763fc11092488d4c110e6be22cd45
Pyae Sone Han <pyaesonehan246810@gmail.com>
2026-04-02 14:50:08 +0700

    Add Starting View

---
+++ SabaiSplit/App/StartingView.swift
@@ -0,0 +1,18 @@
+//
+//  StartingView.swift
+//  SabaiSplit
+//
+//  Created by Dylan on 2/4/26.
+//
+
+import SwiftUI
+
+struct StartingView: View {
+    var body: some View {
+        Text("Starting View")
+    }
+}
+
+#Preview {
+    StartingView()
+}
